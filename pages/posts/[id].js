99278708d4ac85bc923251f417863cfe009ba525
Kim Myungseong <forwarm5891@gmail.com>
2022-04-24 06:40:47 +0900

    feat: feature Make 사용자 지정 404 page.

--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -3,6 +3,12 @@ import { getAllPostIds, getPostData } from '../../lib/posts'
 import Head from 'next/head'
 import Date from '../../components/date'
 import utilStyles from '../../styles/utils.module.css'
+import {useRouter} from 'next/router'
+import { useEffect } from 'react'
+
+
+
+
 export async function getStaticProps({ params }) {
   const postData = await getPostData(params.id)
   return {
@@ -12,6 +18,7 @@ export async function getStaticProps({ params }) {
   }
 }
 export async function getStaticPaths() {
+  // [{ params: { id: pre-rendering}}, ...]
   const paths = getAllPostIds();
     // Returns an array that looks like this:
   // [
@@ -28,10 +35,23 @@ export async function getStaticPaths() {
   // ]
   return {
     paths,
-    fallback: false
+    fallback: false // getStaticPath 에서 주지 않은 id는 404로 처리한다.
   }
 }
 export default function Post({ postData }) {
+  const router = useRouter();
+
+  useEffect(()=>{
+    const getText = async () => {
+      const res = await fetch('/api/hello')
+      const data = await res.json()
+    }
+    getText()
+  },[])
+
+  if(router.isFallback){
+    return <div>Loading....</div>;
+  }
   return (
     <Layout>
       <Head>
--- pages/posts/first-post.jsx
+++ pages/posts/first-post.jsx
@@ -1,8 +1,18 @@
 import Head from 'next/head'
 import Link from 'next/link'
+import Router, { useRouter } from 'next/router'
+import { useEffect } from 'react'
 import Layout from '../../components/layout'
 
 export default function FirstPost() {
+  const router = useRouter();
+  useEffect(()=>{
+    router.push("/posts/first-post/?counter=10",undefined,{ shallow: true })
+  },[])
+
+  useEffect(()=>{
+    alert(router.query.counter)
+  },[router.query.counter])
   return (
     
     <Layout>
